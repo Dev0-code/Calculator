b10753b827db2f2450c8b4b6b552f4eb20e3f0da
Amal Nath K <amalnathk03@gmail.com>
2024-05-29 14:32:30 +0530

    Calculator

---
+++ js.js
@@ -0,0 +1,16 @@
+function displayData(n){
+    result.value+=n
+}
+function findOut(){
+    if(result.value==""){
+        result.value=""
+    }else{
+    result.value=eval(result.value)
+    }
+}
+function clearScreen(){
+    result.value=""
+}
+function backSpace(){
+    result.value=(result.value).slice(0,-1)
+}
